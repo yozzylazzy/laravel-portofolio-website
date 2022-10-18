4646a2d89f3934ef869706bc67fc4f71aceb1d19
Yosef Adrian <kristian_yosef@yahoo.com>
2022-10-18 19:41:07 +0700

    update 404 not found

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -10,38 +10,63 @@
     body {
         background-attachment: fixed;
         background-image: url("assets/img/pola.png");
-        backdrop-filter: blur(5px);
+        backdrop-filter: blur(8px);
         background-repeat: repeat;
         font-family: 'Gemunu Libre', sans-serif;
     }
-    .head-no{
+
+    .head-no {
         font-size: 5em;
         font-weight: bold;
         color: #000;
         font-family: 'Gemunu Libre', sans-serif;
     }
-    .body-no{
+
+    .body-no {
         font-size: 1.6em;
         font-weight: bold;
         color: #000;
         font-family: 'Gemunu Libre', sans-serif;
     }
+    img {
+        vertical-align: text-bottom;
+        text-align: center;
+        margin-left: auto;
+        margin-right: auto;
+        display: block;
+        width: 40em;
+    }
 </style>
 
 <body>
-    <div class="container mt-5">
-        <div class="text-center">
-            <div class="head-no">
-                <p>404 NOT FOUND</p>
-            </div>
-            <div class="body-no">
-                <p>Sorry, the page you are looking for could not be found.</p>
+    <div class="alert">
+        <div class="alert-head">
+            <div class="container mb-5">
+                <div class="alert_img">
+                    <img src="assets/img/404.png" class="img-fluid" alt="Not Found zzz">
+                </div>
+                <div class="text-center">
+                    <div class="head-no">
+                        <p>404 NOT FOUND</p>
+                    </div>
+                    <div class="body-no">
+                        <p>Sorry, the page you are looking for could not be found.</p>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="container">
-        <div class="text-center">
-            <a href="/" class="btn btn-primary">Back to Home</a>
+        <div class="alert-body">
+            <div class="container mb-5">
+                <div class="text-center">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="d-grid gap-2">
+                                <a href="/" class="btn btn-dark p-3" style="font-size: 1.5em;">Home</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
